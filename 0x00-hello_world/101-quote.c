efb3dbae5210b7074693b6e6eceac22bfa7d537e
Alejandro <1249@holbertonschool.com>
2019-09-18 22:35:18 +0000

    C basics v1 - Advance

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
+	return (1);
 }
 

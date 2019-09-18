d7af48f1230df13c8a3fa3ceda2adc5b78cb0ccd
Alejandro <1249@holbertonschool.com>
2019-09-18 20:00:13 +0000

    C basics v1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char dummie[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(dummie);
+
+	return (0);
+}
+

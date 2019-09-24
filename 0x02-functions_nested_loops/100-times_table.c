8816cce0eeb1a1bf5d4e374467825f648c515eb5
Alejandro <1249@holberton.com>
2019-09-24 19:09:03 +0000

    C functions and loops - Advance

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - print a multiply table of size n
+ * @n: Gives the size of the table
+ *
+ *
+ * Return: table sized
+ * On error: -1 inapropiate entry
+ */
+
+void print_times_table(int n)
+{
+	int i = 0;
+	int j = 0;
+	int digits[2];
+	int spaceChar = -16;
+	int result;
+
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+
+			digits[0] = result / 100;
+			digits[1] = (result / 10) % 10;
+			digits[2] = result % 10;
+
+			if (digits[0] == 0)
+				digits[0] = spaceChar;
+
+			if (digits[1] == 0 && digits[0] == spaceChar)
+				digits[1] = spaceChar;
+
+			if (j != 0)
+				_putchar(digits[0] + '0');
+
+			_putchar(digits[1] + '0');
+			_putchar(digits[2] + '0');
+
+			if (j < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}

df1b328dc74768018ed050cde3314e62e7fb9a56
Alejandro <1249@holberton.com>
2019-09-26 22:09:20 +0000

    C functions and loops v2 - Advance

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+
+/**
+ * pot - gives power of a number
+ * @base: base
+ * @opr: power
+ *
+ *
+ * Return: power of base given
+ * On error: -1 inapropiate entry
+ */
+
+int pot(int base, int opr)
+{
+	int i = 0;
+	int result = base;
+
+	for (i = 1; i < opr; i++)
+	{
+		result *= base;
+	}
+
+	if (opr < 1)
+		result = 1;
+
+	return (result);
+}
+
+/**
+ * print_number - print numbers by char
+ * @n: number input
+ *
+ *
+ * Return: nothing
+ * On error: -1 inapropiate entry
+ */
+
+void print_number(int n)
+{
+	int cont = 1;
+	int tmp = 0;
+	int i = 0;
+	int digit = 0;
+
+	tmp = (n < 0) ? n * (-1) : n;
+
+	while (tmp >= 10)
+	{
+		tmp /= 10;
+		cont++;
+	}
+
+	tmp = (n < 0) ? n * (-1) : n;
+
+	if (n < 0)
+		_putchar('-');
+
+	for (i = cont - 1; i >= 0; i--)
+	{
+		if (i == cont - 1)
+			digit = (tmp / pot(10, i));
+		else
+			digit = (tmp / pot(10, i)) % 10;
+
+		_putchar(digit + '0');
+	}
+}

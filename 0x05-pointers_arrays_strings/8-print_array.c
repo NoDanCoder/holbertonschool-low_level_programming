8cdfee26bb8928cb6586d9b8b9b4e87120008273
Alejandro <wetgou@gmail.com>
2019-10-01 07:09:52 -0500

    C pointers, func and arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,6 +15,6 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n; a++, i++)
-		printf((i < n - 1) ? "%d, " : "%d\n", *a);
+	for (i = 0; i < n; i++)
+		printf((i < n - 1) ? "%d, " : "%d\n", *(a + i));
 }

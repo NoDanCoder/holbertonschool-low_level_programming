d1284d99b0ded495b5e4f16f25885fdbfe15a76e
Alejandro <wetgou@gmail.com>
2019-10-03 20:53:21 -0500

    C pointers, func and arrays v2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,13 +11,13 @@
 
 char *leet(char *str)
 {
-	char original[] = {'A', 'E', 'O', 'T', 'L'};
-	char replace[] = {'4', '3', '0', '7', '1'};
+	char original[] = {'A', 'E', 'O', 'T', 'L', '\0'};
+	char replace[] = {'4', '3', '0', '7', '1', '\0'};
 	int cnt = 0;
 	int i = 0;
 
 	for (cnt = 0; *str; str++, cnt++)
-		for (i = 0; i <= 4; i++)
+		for (i = 0; original[i]; i++)
 			if (original[i] == *str || original[i] + 32 == *str)
 			{
 				*str = replace[i];

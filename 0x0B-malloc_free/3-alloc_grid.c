645be46fd814296d97f0976fb700ee4f0faaed62
Alejandro <wetgou@gmail.com>
2019-10-15 12:44:59 -0500

    C calloc and free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,75 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _memset_int - replace from start to "n", the numbers
+ * to int "b", into "s" address
+ * @s: var where changes will be stored
+ * @b: number to set into "s"
+ * @n: delimiter
+ *
+ * like memset, instead of char is for int
+ *
+ * Return: address where is stored the result
+ * On error: -1 inapropiate entry
+ */
+
+int *_memset_int(int *s, int b, unsigned int n)
+{
+	int *org = s;
+	unsigned int i = 0;
+
+	for (; i < n; i++, s++)
+		*s = b;
+
+	return (org);
+}
+
+/**
+ * create_array_int - function that creates an array of int, and
+ * initializes it with a specific value
+ * @size: size of the array
+ * @c: number to set into array elements
+ *
+ * like create_array, instead of char array is for int array
+ *
+ * Return: address where is stored the result
+ * On error: -1 inapropiate entry
+ */
+
+int *create_array_int(unsigned int size, int c)
+{
+	int *arr = malloc(size * sizeof(*arr));
+
+	if (!arr)
+		return (NULL);
+
+	arr = _memset_int(arr, c, size);
+
+	return ((size) ? arr : NULL);
+}
+
+/**
+ * alloc_grid - function that returns a pointer to a 2 dimensional
+ * array of integers, inizialized with 0
+ * @width: width of the matrix
+ * @height: height of the matrix
+ *
+ *
+ * Return: address where is stored the result
+ * On error: -1 inapropiate entry
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **arr = malloc(height * sizeof(int));
+	int i = 0;
+
+	if (!arr)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+		arr[i] = create_array_int(width, 0);
+
+	return (arr);
+}

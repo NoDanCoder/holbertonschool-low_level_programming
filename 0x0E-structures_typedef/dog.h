6bf42eb194aa8a7cbe8845837a34ccd77c64fab3
Alejandro <1249@holbertonschool.com>
2019-10-21 18:52:06 +0000

    C typedef and struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - define a dog object with info of name, age and owner
+ * @name: name of the "dog object"
+ * @age: age of the "dog object"
+ * @owner: owner of the "dog object"
+ *
+ */
+
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);

880611277cd21d48720dd971b8c0d2804840790a
Alejandro <1249@holbertonschool.com>
2019-10-23 17:20:29 +0000

    C pointers to functions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -18,6 +18,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array && action && size > 0)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size > 0)
+	if (array && cmp && size > 0)
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
 				return (i);

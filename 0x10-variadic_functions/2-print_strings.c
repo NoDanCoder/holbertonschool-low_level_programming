dc946db963322961018870bf8d82abcc926ae764
Alejandro Robayo <1249@holbertonschool.com>
2019-10-26 13:56:42 -0500

    C variadic functions - Advance

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,24 +19,25 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_list args;
 
-	if (n == 0)
-		printf("\n");
-		return;
-
 	va_start(args, n);
 
-	for (i = 0; i < (n - 1); i++)
+	if (n > 0)
 	{
+		for (i = 0; i < (n - 1); i++)
+		{
+			str = va_arg(args, char *);
+
+			printf("%s", (!str) ? "(nil)" : str);
+
+			if (separator)
+				printf("%s", separator);
+		}
+
 		str = va_arg(args, char *);
-
 		printf("%s", (!str) ? "(nil)" : str);
-
-		if (separator)
-			printf("%s", separator);
 	}
 
-	str = va_arg(args, char *);
-	printf("%s\n", (!str) ? "(nil)" : str);
+	printf("\n");
 
 	va_end(args);
 }

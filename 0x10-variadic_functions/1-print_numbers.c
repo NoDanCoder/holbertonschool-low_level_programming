cf82664ddb2d34e76875a1c53c00e1b875f8ffc8
Alejandro Robayo <1249@holbertonschool.com>
2019-10-26 13:47:54 -0500

    C variadic functions - Advance

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,15 +18,19 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_list args;
 
+	if (n == 0)
+		printf("\n");
+		return;
+
 	va_start(args, n);
 
 	for (i = 0; i < (n - 1); i++)
 		if (!separator)
-			printf("%ld", va_arg(args, long));
+			printf("%d", va_arg(args, int));
 		else
-			printf("%ld%s", va_arg(args, long), separator);
+			printf("%d%s", va_arg(args, int), separator);
 
-	printf("%ld\n", va_arg(args, long));
+	printf("%d\n", va_arg(args, int));
 
 	va_end(args);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,6 +19,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_list args;
 
+	if (n == 0)
+		printf("\n");
+		return;
+
 	va_start(args, n);
 
 	for (i = 0; i < (n - 1); i++)

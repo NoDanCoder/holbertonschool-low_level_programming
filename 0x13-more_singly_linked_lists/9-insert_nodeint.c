40bd2b4a63b6915eaac96d0fe39c704c862e3825
Alejandro Robayo <1249@holbertonschool.com>
2019-11-05 14:03:09 +0000

    C Singly linked lists v2.0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,8 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - function that inserts a new node at a given position
+ * insert_nodeint_at_index - function that inserts a new node
+ * at a given position
  * @head: double pointer listint_t list to add the element
  * @idx: index to add the element
  * @n: value of the element

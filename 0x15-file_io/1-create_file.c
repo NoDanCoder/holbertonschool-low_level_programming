e087ec0599b8ce65112617751e0323155e6e8c6c
Alejandro Robayo <1249@holbertonschool.com>
2019-11-12 20:56:17 +0000

    C File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,10 +38,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	else if (!text_content)
-		return (1);
 
-	file = open(filename, O_CREAT | O_WRONLY, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 

e6aa46ec09c50162ce23472b54e2d5869d11bad4
Alejandro Robayo <1249@holbertonschool.com>
2019-11-12 19:10:30 -0500

    C File I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,56 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string address
+ *
+ *
+ * Return: nothing
+ * On error: -1 inapropiate entry
+ */
+
+int _strlen(char *s)
+{
+	return ((!*s) ? 0 : 1 + _strlen(s + 1));
+}
+
+/**
+ * append_text_to_file - function that appends text at the end of a file
+ * dont create it if does not exist
+ * @filename: PATH file to append text
+ * @text_content: amount of bytes to append into "filename"
+ *
+ *
+ * Return: 1 on success, -1 on failure (file can not be created, file
+ * can not be written, write “fails”, etc…)
+ * On error: -1 inapropiate entry
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t writeStatus;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	if (!text_content)
+		return (1);
+
+	writeStatus = write(file, text_content, _strlen(text_content));
+	if (writeStatus == -1)
+		return (-1);
+
+	close(file);
+
+	return (1);
+}

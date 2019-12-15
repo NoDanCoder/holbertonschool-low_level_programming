a24e9b9a0e5d4f7b63401353138406e99f6fe292
Alejandro Robayo <1249@holbertonschool.com>
2019-12-15 09:34:04 -0500

    [Fix] add prev node to next one to the added node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,13 +16,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	if (!head)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 
-	new->n = (int) n;
+	new->n = n;
 	new->prev = NULL;
 	new->next = (*head) ? *head : NULL;
+
+	if (*head)
+		(*head)->prev = new;
+
 	*head = new;
 
 	return (new);

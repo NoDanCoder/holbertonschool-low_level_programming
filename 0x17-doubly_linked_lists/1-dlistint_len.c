cbcbe8de93179fbed19e664056a1c0f77cf0ecf4
Alejandro Robayo <1249@holbertonschool.com>
2019-12-14 20:53:27 -0500

    [Add] one: dlistint_len function added

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - function that returns the number of elements
+ * in a linked dlistint_t list
+ * @h: dlistint_t list
+ *
+ * Return: the number of nodes
+ * On error: Nothing
+ *
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t cnt = 0;
+
+	for (; h; h = h->next)
+		cnt++;
+
+	return (cnt);
+}

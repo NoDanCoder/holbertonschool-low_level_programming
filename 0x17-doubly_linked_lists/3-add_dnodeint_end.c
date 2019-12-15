7ef716223f4fd06096ca05de455b67afadba978b
Alejandro Robayo <1249@holbertonschool.com>
2019-12-15 09:52:08 -0500

    [Fix] three: betty style

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,8 +2,8 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - function that adds a new node at the beginning
- * of a dlistint_t list
+ * add_dnodeint_end - function that adds a new node at the end of a
+ * dlistint_t list
  * @head: dlistint_t list address
  * @n: data to insert into new node
  *

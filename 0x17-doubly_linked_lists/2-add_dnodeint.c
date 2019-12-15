fd6d4ec1c6cd28106590fb3171e3030d6094040c
Alejandro Robayo <1249@holbertonschool.com>
2019-12-15 09:17:20 -0500

    [Fix] pass data tp node (int)

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new)
 		return (NULL);
 
-	new->n = n;
+	new->n = (int) n;
 	new->prev = NULL;
 	new->next = (*head) ? *head : NULL;
 	*head = new;

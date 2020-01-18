bc996e80251c6b03302d3da249039efa6e792aed
Alejandro Robayo <1249@holbertonschool.com>
2020-01-18 18:24:47 -0500

    [Fix] zero betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - this functions creates a new hash table
+ * @size: is the size of the array of the table
+ *
+ * Return: a pointer to the newly created hash table
+ * On error: NULL
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hst;
@@ -8,14 +16,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hst = malloc(sizeof(hash_table_t));
 	if (!hst)
-		return NULL;
+		return (NULL);
 
 	buff = malloc(size * sizeof(hash_node_t *));
 	if (!buff)
-		return NULL;
+		return (NULL);
 
 	hst->size = size;
 	hst->array = buff;
 
-	return hst;
+	return (hst);
 }

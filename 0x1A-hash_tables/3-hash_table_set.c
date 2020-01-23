cbe99ee3dcd43560cd0a9f467da75fb0d347d242
Alejandro Robayo <1249@holbertonschool.com>
2020-01-22 22:00:55 -0500

    [Fix] three - existing key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,24 +14,38 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long index;
-	hash_node_t *new;
+	hash_node_t *new, *current;
 	const unsigned char *keyGet = (const unsigned char *)key;
 
 	if (!ht || !(ht->array) || !key || !*key || !value)
 		return (0);
 
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
-		return (0);
-
-	new->key = strdup(key);
-	new->value = strdup(value);
-	if (!(new->value) || !(new->key))
-		return (0);
-
 	index = key_index(keyGet, ht->size);
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	current = ht->array[index];
+	while (current && strcmp(current->key, key) != 0)
+                current = current->next;
+
+	if (!current)
+	{
+		new = malloc(sizeof(hash_node_t));
+		if (!new)
+			return (0);
+
+		new->key = strdup(key);
+		new->value = strdup(value);
+		if (!(new->value) || !(new->key))
+			return (0);
+
+		new->next = ht->array[index];
+		ht->array[index] = new;
+	}
+	else
+	{
+		current->value = strdup(value);
+		if (!(current->value))
+			return (0);
+	}
 
 	return (1);
+
 }

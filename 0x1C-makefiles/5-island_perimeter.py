33e24a430da4f5368d0cfec621e31ebd62788f97
Alejandro Robayo <1249@holbertonschool.com>
2020-02-22 01:25:27 -0500

    [Feature] algorithm simplyfied

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 """ get the perimeter of the island described in grid """
 
+
 def island_perimeter(grid):
     """ Get perimeter of a island inside a matrix """
     total = 0
-
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x]:
-                if indexExist(grid, x-1, y) == 0:
-                    total += 1
-                if indexExist(grid, x+1, y) == 0:
-                    total += 1
-                if indexExist(grid, x, y-1) == 0:
-                    total += 1
-                if indexExist(grid, x, y+1) == 0:
-                    total += 1
-
+                total += indexExist(grid, x-1, y)
+                total += indexExist(grid, x+1, y)
+                total += indexExist(grid, x, y-1)
+                total += indexExist(grid, x, y+1)
     return total
+
 
 def indexExist(grid, x, y):
     """ check if the position is valid """
     try:
         if x < 0 or y < 0:
-            return 0
-        return grid[y][x]
+            return 1
+        return not grid[y][x]
     except IndexError:
-        return 0
+        return 1
